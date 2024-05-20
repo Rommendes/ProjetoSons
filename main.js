b6fb96bc48c4169987fe188ab87046069b047ecb
Rosangela <96451367+Rommendes@users.noreply.github.com>
2024-05-20 14:41:14 -0300

    .

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-/*
-function tocSomPom (){
-    document.querySelector('#som_tecla_pom').play();
-}
-document.querySelector('.tecla_pom').onclick = tocSomPom;
 
-function tocSomClap (){
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = tocSomClap;
-*/
+// function tocSomPom (){
+//     document.querySelector('#som_tecla_pom').play();
+// }
+// document.querySelector('.tecla_pom').onclick = tocSomPom;
 
-function tocSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+// function tocSomClap (){
+//     document.querySelector('#som_tecla_clap').play();
+// }
+// document.querySelector('.tecla_clap').onclick = tocSomClap;
+
+
+function tocSom (idElmentoAudio){
+
+    document.querySelector(idElmentoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onClick = tocSomPom;
+//listaDeTeclas[0].onclick = tocSomPom;
+
+let contador = 0;
+//enquanto
+while(contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocSom;
+    contador++;
+    console.log(contador);
+}
