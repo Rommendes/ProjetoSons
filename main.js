6ebdb8dede30a29b421b0e4b9982e8ebd04fd927
Rosangela <96451367+Rommendes@users.noreply.github.com>
2024-05-20 12:31:29 -0300

    up

---
+++ main.js
@@ -0,0 +1,17 @@
+
+/*
+function tocSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocSomPom;
+
+unction tocSomClap (){
+    document.querySelector('#som_tecla_Clap').play();
+}
+document.querySelector('.tecla_Clap').onclick = tocSomClap
+*/
+function tocSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onClick
